4499e7dd062beee3deb3d548597b01d52e380298
zeyu.qian <qianzeyu@chebada.com>
2024-04-11 14:10:55 +0800

    第一次上传

---
+++ jenkinsDemo/src/main/java/com/example/jenkinsdemo/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.jenkinsdemo.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+    /**
+     * 获取模拟数据
+     */
+    @RequestMapping("/data")
+    @ResponseBody
+    public String data() {
+        System.out.println("---------databbbb----------");
+        return "hello wordbbbbb";
+    }
+}
